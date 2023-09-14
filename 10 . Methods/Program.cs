91ba85033a18d5c858ea53fd7ac54d2f019581f4
user <user@LAPTOP-KAN0NU4I>
2023-09-14 20:54:19 +0200

    Add project files.

---
+++ 10 . Methods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _10.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calls for the sayHi method
+            sayHi();
+
+            // Calls for the Addnumber method
+            AddNumber();
+
+            // You can switch the order
+            //Freeze console
+            Console.ReadLine();
+        }
+
+        static void sayHi()
+        {
+            Console.WriteLine("Your name: ");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name +"\n");
+            Console.WriteLine("Hi " + name + ", sayHi is done \n");
+        }
+
+        static void AddNumber()
+        {
+            Console.WriteLine("Enter your first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("\n"+ "Hi, Add Number is done, you can now get results:");
+
+            Console.WriteLine(num1 + num2 + "\n");
+        }
+    }
+}
